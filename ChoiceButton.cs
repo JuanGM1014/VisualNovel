ec6e652fdaa1ac51e96ae3eb06cf2e6549c988ed
Juan Cristobal <juancris345@gmail.com>
2022-06-14 14:20:21 -0500

    Arbol de desiciones

---
+++ ChoiceButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChoiceButton : MonoBehaviour 
+{
+	public TMP_Text tmpro;
+	public string text {get{return tmpro.text;} set{tmpro.text = value;}}
+
+	[HideInInspector]
+	public int choiceIndex = -1;
+}
